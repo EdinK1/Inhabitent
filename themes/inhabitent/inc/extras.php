faafe135842e2a9b1150401efd26a6141b1cf26c
EdinK1 <edinkaymakchi@gmail.com>
2019-07-11 16:30:35 -0700

    add extras.php

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -20,3 +20,33 @@ function red_starter_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'red_starter_body_classes' );
+
+function custom_login_logo() {
+	?>
+	<style type="text/css">
+	body.login div#login h1 a {
+		background: url(http://localhost/inhabitent/wp-content/themes/inhabitent/images/logos/inhabitent-logo-text-dark.svg) no-repeat center center/contain;
+		padding-bottom: 30px;
+		width: 320px;
+		height: 25px;
+	}
+		
+	body.login div#login form#loginform input#wp-submit {
+	background: rgb(36, 138, 130);
+	}
+	
+	</style>
+	<?php
+	} add_action( 'login_enqueue_scripts', 'custom_login_logo' );
+	?>
+	<?php 
+	add_filter( 'login_headerurl', 'custom_loginlogo_url' );
+
+	function custom_loginlogo_url($url) {
+	
+		 return  'https://localhost/inhabitent';
+	
+	}
+	
+
+	
