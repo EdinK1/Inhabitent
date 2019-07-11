0cc66df39ecec66e6bb6e264e38729f29797aef4
EdinK1 <edinkaymakchi@gmail.com>
2019-07-11 16:29:12 -0700

    add footer.php

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,9 +10,35 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+				<div class="site-info container">
+					<ul>
+						<span class="info-title">Contact Info</span>
+						<li>
+							<a href="mailto:info.inhabitent@gmail.com">
+							<i class="fas fa-envelope"></i> info@inhabitent.com</a>
+						</li>
+						<li>
+							<a href="#">
+							<i class="fas fa-phone-alt"></i> 778-456-7891</a>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-facebook-square"></i></a>
+							<a href="#"><i class="fab fa-twitter-square"></i></a>
+							<a href="#"><i class="fab fa-google-plus-square"></i></a>
+						</li>
+					</ul>
+
+					<ul>
+						<span class="info-title">Business Hours</span>
+						<li><span>Monday-Friday:</span> 9am to 5pm</li>
+						<li><span>Saturday:</span> 10am to 2pm</li>
+						<li><span>Sunday: </span>Closed</li>
+					</ul>
+					<ul class="footer-last">
+						<img class="footer-logo" src="http://localhost/inhabitent/wp-content/themes/inhabitent/images/logos/4.png" alt="">
+					</ul>
+					</div><!-- .site-info -->
+				<p>COPYRIGHT © 2019 INHABITENT</p>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
