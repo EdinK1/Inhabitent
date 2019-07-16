fd9032626ac157cdeb8238643aeaba545cf4bd9b
EdinK1 <edinkaymakchi@gmail.com>
2019-07-16 12:21:37 -0700

    create single page

--- themes/inhabitent/single.php
+++ themes/inhabitent/single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts.
+ * The main template file.
  *
  * @package RED_Starter_Theme
  */
@@ -10,21 +10,28 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+		<?php if ( have_posts() ) : ?>
 
-			<?php the_post_navigation(); ?>
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+				<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+				</header>
+			<?php endif; ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/content-single' ); ?>
+			<?php endwhile; ?>
 
-		<?php endwhile; // End of the loop. ?>
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
