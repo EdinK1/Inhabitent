3c2a8bacd8460659c6afdc99657e9a44db362401
EdinK1 <edinkaymakchi@gmail.com>
2019-07-16 12:15:05 -0700

    create archive page

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -10,35 +10,59 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php
+		$args = array('numberposts' => 16,
+			'post_type' => 'product',
+			'order'       => 'ASC');
+			$posts = get_posts($args);
+		?>
 
-			<header class="page-header">
-				<?php
-				?>
-			</header><!-- .page-header -->
+
+		<?php if ( ! empty($posts) ) : ?>
+
 
 			<div class="container">
+			<header class="page-header">
+			<h1 class="shop-title">Shop Stuff</h1>
+			<?php $terms = get_terms(array(
+            'taxonomy' => 'product_type',
+          ));?>
+
+        <ul class="shop-terms">
+          <?php if ($terms) : foreach ($terms as $term) :?>
+              
+			<li class="entry-title">
+			<a href="<?= esc_url(get_term_link($term)); ?>">
+                <?= $term->name; ?>
+				</a>
+              </li>
+
+          <?php endforeach; endif;?>
+        </ul>
+
+			</header><!-- .page-header -->
+
 			<section class="shop">
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
 
 				<?php
 					get_template_part( 'template-parts/content-product' );
 				?>
+			<?php endforeach; wp_reset_postdata(); ?>
 
-			<?php endwhile; ?>
 
 			</section>
 			</div>
 
-			<?php the_posts_navigation(); ?>
 
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
