602b73dc1ed418e490c8bb67cb59b15e541237bb
EdinK1 <edinkaymakchi@gmail.com>
2019-07-16 23:00:14 -0700

    create specific template for the front page

---
+++ themes/inhabitent/template-parts/content-front-page-journal.php
@@ -0,0 +1,16 @@
+		<li>
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+		<?php endif; ?>
+
+		<div class="journal-desc">			
+		<?php if ( 'post' === get_post_type() ) : ?>
+			<p class="journal-date">
+			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
+		</p>
+		<?php endif; ?>
+
+		<?php the_title( sprintf( '<h2 class="journal-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<a class="btn" href="<?php echo esc_url(get_permalink()); ?>">Read Entry</a>
+	</div>
+	</li>
