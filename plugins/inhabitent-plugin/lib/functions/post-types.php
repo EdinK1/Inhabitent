674c0c543d8b3e4e7cde68fb014ab2b85dc291a9
EdinK1 <edinkaymakchi@gmail.com>
2019-07-18 00:08:23 -0700

    add the adventures archive

--- plugins/inhabitent-plugin/lib/functions/post-types.php
+++ plugins/inhabitent-plugin/lib/functions/post-types.php
@@ -65,4 +65,59 @@ function inhabitent_cpt_product() {
         );
     register_post_type( 'product', $args );
  }
+
+ function adventures() {
+
+	$labels = array(
+		'name'                  => _x( 'Adventures', 'Post Type General Name', 'adventures' ),
+		'singular_name'         => _x( 'Adventure', 'Post Type Singular Name', 'adventures' ),
+		'menu_name'             => __( 'Adventures', 'adventures' ),
+		'name_admin_bar'        => __( 'Post Type', 'adventures' ),
+		'archives'              => __( 'Item Archives', 'adventures' ),
+		'attributes'            => __( 'Item Attributes', 'adventures' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'adventures' ),
+		'all_items'             => __( 'All Items', 'adventures' ),
+		'add_new_item'          => __( 'Add New Item', 'adventures' ),
+		'add_new'               => __( 'Add New', 'adventures' ),
+		'new_item'              => __( 'New Item', 'adventures' ),
+		'edit_item'             => __( 'Edit Item', 'adventures' ),
+		'update_item'           => __( 'Update Item', 'adventures' ),
+		'view_item'             => __( 'View Item', 'adventures' ),
+		'view_items'            => __( 'View Items', 'adventures' ),
+		'search_items'          => __( 'Search Item', 'adventures' ),
+		'not_found'             => __( 'Not found', 'adventures' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'adventures' ),
+		'featured_image'        => __( 'Featured Image', 'adventures' ),
+		'set_featured_image'    => __( 'Set featured image', 'adventures' ),
+		'remove_featured_image' => __( 'Remove featured image', 'adventures' ),
+		'use_featured_image'    => __( 'Use as featured image', 'adventures' ),
+		'insert_into_item'      => __( 'Insert into item', 'adventures' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'adventures' ),
+		'items_list'            => __( 'Items list', 'adventures' ),
+		'items_list_navigation' => __( 'Items list navigation', 'adventures' ),
+		'filter_items_list'     => __( 'Filter items list', 'adventures' ),
+	);
+	$args = array(
+		'label'                 => __( 'Adventure', 'adventures' ),
+		'description'           => __( 'adventures description', 'adventures' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'adventures', $args );
+
+}
+ add_action( 'init', 'adventures', 0 );
  add_action( 'init', 'inhabitent_cpt_product' );
