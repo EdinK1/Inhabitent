4c2ada6e4f56e5f3aa8fd77f9078290f635d7a0b
EdinK1 <edinkaymakchi@gmail.com>
2019-07-18 00:18:11 -0700

    create a specif template to loop over the adventures archive in the front page

--- themes/inhabitent/template-parts/content-front-page-journal.php
+++ themes/inhabitent/template-parts/content-front-page-journal.php
@@ -4,13 +4,7 @@
 		<?php endif; ?>
 
 		<div class="journal-desc">			
-		<?php if ( 'post' === get_post_type() ) : ?>
-			<p class="journal-date">
-			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
-		</p>
-		<?php endif; ?>
-
 		<?php the_title( sprintf( '<h2 class="journal-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-		<a class="btn" href="<?php echo esc_url(get_permalink()); ?>">Read Entry</a>
+		<a class="btn" href="<?php echo esc_url(get_permalink()); ?>">Read More</a>
 	</div>
 	</li>
