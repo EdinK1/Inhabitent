d8abe2041d20ce200b850115885477e84aab2f6e
EdinK1 <edinkaymakchi@gmail.com>
2019-07-22 20:57:47 -0700

    wrap the links in a div

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -35,9 +35,12 @@
 					</a>
 				</div>	
 				
+				<div class="links">
 				<button class="menu-toggle" aria-controls="primary-menu"
 						aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					<?= get_search_form(); ?>
+				</div>
 				</nav><!-- #site-navigation -->
 		</header><!-- #masthead -->
 
