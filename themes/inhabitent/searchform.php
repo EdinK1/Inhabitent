02ca960858d7fad974faccde86fe9f2a63f91dc6
EdinK1 <edinkaymakchi@gmail.com>
2019-07-22 21:00:10 -0700

    get rid of the link

--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -1,10 +1,8 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
-		<a href="#">
-			<i class="fa fa-search"></i>
-		</a>
 		<label>
-		<input type="search" class="search" placeholder="Type and hit enter..." value="" name="s" title="Search for:">
+		<i class="fa fa-search"></i>
+		<input required type="search" class="search" placeholder="Type and hit enter..." value="" name="s" title="Search for:">
 		</label>
 			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
 		</button>
